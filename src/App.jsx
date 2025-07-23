d08a6ca9569cb5cbfacc95bb45ef6484fbc48787
highonhues <ananya1.sci@gmail.com>
2025-07-22 23:41:43 -0700

    header finished and so ami

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 // const fun declares app and then wrap it
 import Button from "./components/Button";
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="#login">
-          himytry
-        </Button>
+        <Header />
       </div>
       <ButtonGradient />
     </>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,13 +3,26 @@
 import ButtonSvg from "../assets/svg/ButtonSvg";
 
 const Button = ({ className, href, onClick, children, px, white }) => {
+  const classes = `button relative inline-flex justify-center h-11 items-center transition-colors hover:text-color-1 ${
+    px || "px-7"
+  } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
+
+  const spanClasses = "relative z-10";
+
   const renderButton = () => (
-    <button>
-      <span>{children}</span>
+    <button className={classes}>
+      <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </button>
   );
-  return renderButton();
+
+  const renderLink = () => (
+    <a href={href} className={classes}>
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </a>
+  );
+  return href ? renderLink() : renderButton();
 };
 
 export default Button;
